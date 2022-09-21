38884b31246885a63fd05b1f0bc4d88105a653a9
root <Ammar Abd Alfattah3>
2022-09-21 16:40:03 +0000

    printing a different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for num1 in range(0, 10):
     for num2 in range(0, 10):
         if num1 < num2:
             rx = "{:d}{:d}".format(num1, num2)
-            print(rx, end="" if num1 * num2 == 72 else ", ")
+            print(rx, end="\n" if num1 * num2 == 72 else ", ")
