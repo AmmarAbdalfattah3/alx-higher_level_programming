8961a536b58b6488908a0b21acd206f6ec02c257
root <Ammar Abd Alfattah3>
2022-09-21 00:08:33 +0000

    Print all the letters except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z') + 1):
-    if chr(letter) != 'p' or chr(letter) != 'e':
-        print("{:c}".format(letter), end="")
+    char = "{:c}".format(letter)
+    if char != 'p' and char != 'e':
+        print(char, end="")
