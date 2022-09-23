7acc24e5462591e081514278de23b1c16c999061
root <Ammar Abd Alfattah3>
2022-09-23 00:25:05 +0000

    modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    len_argv = len(argv) - 1
-    print("{:d}".format(len_argv))
+    number = 0
+    for string in argv[1:]:
+        number += int(s)
+        print("{:d}".format(number))
