665cc02924ce71bdf495b0c2402ff49b40a9f464
root <Ammar Abd Alfattah3>
2022-09-23 14:09:35 +0000

    modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for string in dir(hidden_4)
+    rx = dir(hidden_4)
+    for string in rx
         if string[:2] != "__":
             print("{:s}".format(string))
