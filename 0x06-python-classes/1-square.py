a80b8613c1a1dcb80825461c952991a50e82a61c
root <Ammar Abd Alfattah3>
2022-10-12 00:04:42 +0000

    a size square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""defining a square class"""
+
+class Square:
+    """Represents a square
+
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size_square):
+        """Initializes a square
+
+        Args:
+            size (int): size of a side of the square
+
+        Returns: None
+        """
+        self.__size = size_square
+
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,1 +1,1 @@
-0-square.py defines a empty square class
+0-square.py defines a empty square class.
