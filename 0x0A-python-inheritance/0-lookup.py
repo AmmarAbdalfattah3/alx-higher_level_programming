15ee89e86d07c57af1ac812ab074c9dfe86115be
root <Ammar Abd Alfattah3>
2022-10-25 21:34:19 +0000

    object attribute.

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defining an object attribute."""
+
+
+def lookup(obj):
+    """Return the list of available attributes and methods of an object."""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+0-lookup.py returns the list of available attributes and methods of an object.
