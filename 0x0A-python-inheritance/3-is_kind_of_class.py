d9507b20d1dfa89b6112160790f88fecc7bb9b83
root <Ammar Abd Alfattah3>
2022-10-26 02:00:52 +0000

    Checking the object

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module contains the is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True or False"""
+    return (isinstance(obj, a_class))
