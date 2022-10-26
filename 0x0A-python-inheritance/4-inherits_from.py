63ce3a4ab0517aee8b0a85569c73b223269a8b05
root <Ammar Abd Alfattah3>
2022-10-26 13:41:21 +0000

    Checking the object

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains the inherits_from function"""
 
+
 def inherits_from(obj, a_class):
     """Return True or False"""
     return(issubclass(type(obj), a_class) and type(obj) != a_class)
