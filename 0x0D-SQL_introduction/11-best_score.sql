55bbf01a9aa5f751d8541061e26cf23d5d9b075c
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2022-12-01 20:42:15 +0000

    lists all records with a score >= 10 in the table

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,6 @@
--- Updates the score of Bob to 10 in the table second_table.
-UPDATE `second_table`
-SET `score` = 10
-WHERE `name` = "Bob";
+-- Lists all records in the table second_table with a score >= 10.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
