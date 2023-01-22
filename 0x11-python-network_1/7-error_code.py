23ee09f0359b788d599a25a8c572a61724a6b200
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2023-01-22 06:13:21 -0800

    python network 1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,9 +6,9 @@
 import requests
 import sys
 if __name__ == "__main__":
-	url = sys.argv[1]
-	res = requests.get(url)
-	if res.status_code >= 400:
-		print(f"Error code: {res.status_code}")
-	else:
-		print(res.text)
+    url = sys.argv[1]
+    res = requests.get(url)
+    if res.status_code >= 400:
+        print(f"Error code: {res.status_code}")
+    else:
+        print(res.text)
