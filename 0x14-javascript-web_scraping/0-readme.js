e61de6f59db3dfee6c31febdb987a47617632dbd
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2023-02-09 11:11:07 -0800

    web scraping tasks

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-var file_arguments = process.argv[2];
-fs.readFile(file_arguments, 'utf-8', function(Error, data) {
+
+fs.readFile(process.argv[2], 'utf-8', function(Error, data) {
   if (Error) {
     console.log(Error); 
   }
