c02070b478cb07d1e72759994201516f00027cdd
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2023-02-10 05:13:27 -0800

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+'
+request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   }
