2a89cf9cb3c9ecb29b8f07ceb9bcf07228abdc82
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-02-11 09:46:09 +0200

    mylist

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module for Mylist class"""
+
+
+class MyList(list):
+    """a child of List class"""
+    def __init__(self):
+        """initializes the class instance"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the list, but sorted"""
+        print(sorted(self))
