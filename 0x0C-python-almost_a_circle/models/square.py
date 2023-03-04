e22d149d11930c039a39b6e8bcdf2c95328b16ce
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2023-03-04 00:52:00 -0800

    sq

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,43 +5,43 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-	"""a class that creates squres"""
+    """a class that creates squres"""
 
-	def __init__(self, size, x=0, y=0, id=None):
-		"""initialilzer for Rectangle class to be
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialilzer for Rectangle class to be
            called when an istance is instantiated
         """
-		super().__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
-	@property
-	def size(self):
-		"""size attribute getter method"""
-		return self._size
+    @property
+    def size(self):
+        """size attribute getter method"""
+        return self._size
 
-	@size.setter
-	def size(self, value):
-		"""size attribute setter method"""
-		self.width = value
-		self.height = value
+    @size.setter
+    def size(self, value):
+        """size attribute setter method"""
+        self.width = value
+        self.height = value
 
-	def update(self, *args, **kwargs):
-		""""updates a Square instance with new attributes"""
-		if args:
-			index = 0
-			for arg in args:
-				if index == 1:
-					self.id = arg
-				elif index == 2:
-					self.size(arg)
-				elif index == 3:
-					self.x = arg
-				elif index == 4:
-					self.y = arg
-				index += 1
-		elif kwargs:
-			for key, value in kwargs.items():
-				if k == "id":
-                	self.id = value
+    def update(self, *args, **kwargs):
+        """"updates a Square instance with new attributes"""
+        if args:
+            index = 0
+            for arg in args:
+                if index == 1:
+                    self.id = arg
+                elif index == 2:
+                    self.size(arg)
+                elif index == 3:
+                    self.x = arg
+                elif index == 4:
+                    self.y = arg
+                index += 1
+        elif kwargs:
+            for key, value in kwargs.items():
+                if k == "id":
+                    self.id = value
                 elif k == "size":
                     self.size(value)
                 elif k == "x":
@@ -58,9 +58,6 @@ class Square(Rectangle):
             "y": self.y
         }
 
-
-
-	def __str__(self):
-        """prints string representing Square instance when printed"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-		
+        def __str__(self):
+            """prints string representing Square instance when printed"""
+            return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
