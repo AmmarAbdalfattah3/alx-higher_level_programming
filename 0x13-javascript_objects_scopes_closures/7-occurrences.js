0c7a8b9ae9a152824cee5f861d7948b56bc5066c
AmmarAbdalfattah3 <106756961+AmmarAbdalfattah3@users.noreply.github.com>
2024-03-02 19:29:36 +0200

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-  return list.reduce((num, current) => current === searchElement ? num + 1 : num, 0);
-};
--- 0x13-javascript_objects_scopes_closures/README.md
+++
@@ -1,1 +0,0 @@
-0-rectangle.js defines a rectangle
