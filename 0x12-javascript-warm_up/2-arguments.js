0bcdf160a6b34badde88d6a1d477dbc33a7954bd
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-03-11 16:51:42 -0700

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+if (process.argv[0])
+{
+	console.log('No argument');
+}
+else if (process.argv[1])
+{
+	console.log('Arguments found');
+}
+else
+{
+	console.log('Arguments found');
+}
