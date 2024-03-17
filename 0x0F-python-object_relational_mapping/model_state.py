e56068897b78c93b067f832e11a154248241e773
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-03-17 14:48:15 -0700

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""This module contains the class definition of a `State`
-   and an instance `Base` = `declarative_base()`.
+"""This module file that contains the class definition of a State
+   and an instance Base = declarative_base()
 """
 
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
 
+
 class State(Base):
-    """This class represents a `states` table
+    """This class represents the `states` table
        in SQL database
     """
 
